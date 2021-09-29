453006c11b9d27ff744b0c838049dd1c6c2d646e
liitlelenim <jakubblaszczyk20@gmail.com>
2021-09-29 18:44:43 +0200

    create thought model

---
+++ src/main/java/me/littlelenim/mythought/thought/model/Thought.java
@@ -0,0 +1,67 @@
+package me.littlelenim.mythought.thought.model;
+
+import org.hibernate.Hibernate;
+
+import javax.persistence.*;
+import java.util.Date;
+import java.util.Objects;
+
+@Entity
+@Table(name = "thought")
+public class Thought {
+    @SequenceGenerator(
+            name = "thought_sequence",
+            sequenceName = "thought_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(generator = "thought_sequence")
+    @Id
+    @Column(name = "id", unique = true, updatable = false, nullable = false)
+    private Long id;
+    @Column(name = "content", nullable = false)
+    private String content;
+    @Column(name = "post_date", nullable = false)
+    private Date postDate;
+
+    public Thought(String content) {
+        this.content = content;
+        this.postDate = new Date();
+    }
+
+    public Thought() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Date getPostDate() {
+        return postDate;
+    }
+
+    public void setPostDate(Date postDate) {
+        this.postDate = postDate;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        Thought thought = (Thought) o;
+        return Objects.equals(id, thought.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return 0;
+    }
+}
