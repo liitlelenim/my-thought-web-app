db7cfc6f62f257a584917bd66d5ac7ce643ff648
liitlelenim <jakubblaszczyk20@gmail.com>
2021-09-29 18:58:44 +0200

    create tag model

---
+++ src/main/java/me/littlelenim/mythought/thought/model/Tag.java
@@ -0,0 +1,54 @@
+package me.littlelenim.mythought.thought.model;
+
+import org.hibernate.Hibernate;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tag")
+public class Tag {
+    @SequenceGenerator(
+            name = "tag_sequence",
+            sequenceName = "tag_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(generator = "tag_sequence")
+    @Id
+    @Column(name = "id", unique = true, nullable = false, updatable = false)
+    private Long id;
+    @Column(name = "name", unique = true, nullable = false, updatable = false)
+    private String name;
+
+    public Tag(String name) {
+        this.name = name;
+    }
+
+    public Tag() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        Tag tag = (Tag) o;
+        return Objects.equals(id, tag.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return 0;
+    }
+}
