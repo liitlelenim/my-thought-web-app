0fe96d20c177b95d40fc9c1876f975e5262f2685
liitlelenim <jakubblaszczyk20@gmail.com>
2021-09-29 18:33:58 +0200

    initial commit

---
+++ src/main/java/me/littlelenim/mythought/MyThoughtApplication.java
@@ -0,0 +1,13 @@
+package me.littlelenim.mythought;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyThoughtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyThoughtApplication.class, args);
+    }
+
+}
