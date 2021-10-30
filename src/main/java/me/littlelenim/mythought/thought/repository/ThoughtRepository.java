616f926d39684b2f755931d68536a29f63a91289
liitlelenim <jakubblaszczyk20@gmail.com>
2021-10-30 17:24:11 +0200

    fixed hibernate inefficiency warn

--- src/main/java/me/littlelenim/mythought/thought/repository/ThoughtRepository.java
+++ src/main/java/me/littlelenim/mythought/thought/repository/ThoughtRepository.java
@@ -14,21 +14,21 @@ import java.util.Optional;
 @Repository
 public interface ThoughtRepository extends JpaRepository<Thought, Long> {
 
-    @Query("select t , c from Thought t left join fetch t.comments c  where t.id = ?1")
+    @Query("select distinct t , c from Thought t left join fetch t.comments c  where t.id = ?1")
     Optional<Thought> findByIdAndJoinCommentsAndLikes(Long id);
 
-    @Query("select t, l from Thought t left join fetch t.likedBy l where t.id =?1")
+    @Query("select distinct t, l from Thought t left join fetch t.likedBy l where t.id =?1")
     Optional<Thought> findByIdAndJoinLikes(Long id);
 
-    @Query("select t,l from Thought t left join fetch t.likedBy l order by t.postDate DESC")
+    @Query("select distinct t,l from Thought t left join  t.likedBy l order by t.postDate DESC")
     List<Thought> findByOrderByPostDateDesc(Pageable pageable);
 
     @Query("select t from Thought  t where :tag member t.tags order by t.postDate DESC")
     List<Thought> findByTagOrderByPostDateDesc(Pageable pageable, @Param("tag") Tag tag);
 
-    @Query("select t from Thought  t where :username = t.author.username order by t.postDate DESC")
+    @Query("select  t from Thought  t where :username = t.author.username order by t.postDate DESC")
     List<Thought> findByUsernameOrderByPostDateDesc(Pageable pageable, @Param("username") String username);
 
-    @Query("select t,u from Thought t left join fetch  t.author u where t=?1 ")
+    @Query("select distinct t,u from Thought t left join fetch  t.author u where t=?1 ")
     Thought getThoughtWithAuthor(Thought thought);
 }
